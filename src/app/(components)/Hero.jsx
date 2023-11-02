3b2eced68944b0cbd545ff07c642215b7287b688
Kevin Yven Riexinger <info@kevinriexinger.de>
2023-11-02 22:48:41 +0100

    better hero

--- src/app/(components)/Hero.jsx
+++ src/app/(components)/Hero.jsx
@@ -1,17 +1,16 @@
 export default function Hero() {
   return (
     <>
-      <div class="px-4 py-5 my-5 text-center">
+      <div className="px-4 py-5 my-5 text-center" data-bs-theme="light">
         <img
-          class="d-block mx-auto mb-4"
-          src="/wappen.jpg"
+          className="d-block mx-auto mb-4"
+          src="/wappen.svg"
           alt="Das Wappen der Stadt Ratingen"
           height="250em"
-          width="250em"
         />
-        <h1 class="display-5 fw-bold text-body-emphasis">Stadt Ratingen</h1>
-        <div class="col-lg-8 mx-auto">
-          <p class="lead mb-4">
+        <h1 data-bs-theme="light" className="display-5 fw-bold text-body-emphasis">Stadt Ratingen</h1>
+        <div className="col-lg-8 mx-auto">
+          <p className="lead mb-4 mt-4">
           Liebe Besucherinnen und Besucher der Internetseite der Stadt Ratingen,<br /><br />
 Ihre gewohnte Website www.ratingen.de ist aktuell nicht erreichbar. Hintergrund ist ein Cyber-Angriff auf den kommunalen Dienstleister Südwestfalen IT (SIT), der auch die Website der Stadt Ratingen hostet. Anders als in anderen Städten und Kreisen in NRW, in denen die gesamte Verwaltungs-IT ausgefallen ist, sind die Auswirkungen in Ratingen begrenzt und beschränken sich im Wesentlichen auf die Website. Die Verwaltungs-IT in Ratingen wird nicht durch die SIT betrieben und funktioniert daher.<br /><br />
 
@@ -19,13 +18,13 @@ Auch Online-Dienstleistungen der Stadtverwaltung, etwa Terminbuchungen im Bürge
 
 Wie lange die Störung anhält, ist zurzeit nicht absehbar. Die SIT arbeitet unter Hochdruck an der Behebung.<br /><br />
 
-Stadt Ratingen
+<em>Stadt Ratingen</em>
           </p>
-          {/* <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
+          {/* <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
               Primary button
             </button>
-            <button type="button" class="btn btn-outline-secondary btn-lg px-4">
+            <button type="button" className="btn btn-outline-secondary btn-lg px-4">
               Secondary
             </button>
           </div>  */}
