e6c449b7d618e85281829f45636ddc49556546f1
kdev <konstantinw@ledbrain.de>
2023-11-02 22:15:34 +0100

    fix: base path errors

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/notfall.ratingen.de',
-    images: {
-        unoptimized: true,
-    },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
