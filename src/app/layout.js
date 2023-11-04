d79c77ee922413affc7d3d2deb52bdc386172975
Kevin Yven Riexinger <info@kevinriexinger.de>
2023-11-05 00:36:56 +0100

    added og:xxx

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,39 @@
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 
-import '@fortawesome/fontawesome-svg-core/styles.css';
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
-import { config } from '@fortawesome/fontawesome-svg-core';
+import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 
-import '@/styles/styles.scss';
+import "@/styles/styles.scss";
 
-import Footer from './(components)/Footer';
+import Footer from "./(components)/Footer";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: 'Stadt Ratingen',
-    description: '',
+  title: "Stadt Ratingen",
+  description: "",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="de" data-bs-theme="light">
-            <body data-bs-theme="light" className={inter.className}>
-                {children}
-                <Footer />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="de" data-bs-theme="light">
+      <head>
+        <meta property="og:title" content="Stadt Ratingen" />
+        <meta
+          property="og:description"
+          content="Die Backup-Website der Stadt Ratingen"
+        />
+        <meta
+          property="og:image"
+          content="/wappen.jpg"
+        />
+      </head>
+      <body data-bs-theme="light" className={inter.className}>
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
 }
