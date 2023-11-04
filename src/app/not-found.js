243c184fe041df05dbe4137805e0cbb1ea8a2128
kdev <konstantinw@ledbrain.de>
2023-11-04 23:52:36 +0100

    refactor: simplify not-found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,5 +1,23 @@
-import NotFound from './(components)/NotFound';
+import Link from 'next/link';
 
-export default function FourOhFour() {
-    return <NotFound />;
+export default function NotFound() {
+    return (
+        <div className="px-4 py-5 my-5 text-center" data-bs-theme="light">
+            <img
+                className="d-block mx-auto mb-4"
+                src="/wappen.svg"
+                alt="Das Wappen der Stadt Ratingen"
+                height="250em"
+            />
+            <h1
+                data-bs-theme="light"
+                className="display-5 fw-bold text-body-emphasis"
+            >
+                Seite nicht gefunden
+            </h1>
+            <Link href="/" className="btn btn-primary mt-5">
+                Zur Startseite
+            </Link>
+        </div>
+    );
 }
