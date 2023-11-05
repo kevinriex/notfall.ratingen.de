2268355ae21a4e7141ad08c3149e27d3468542dd
Konstantin W <konstantinw@ledbrain.de>
2023-11-05 22:38:26 +0100

    Merge `dev` into `main` (#6)
    * Rebase dev branch (#3)
    
    * changed vercel prod to run on main
    
    * out
    
    * added custom 404 page
    
    * refactor: tidy up codebase a bit
    
    * changed parameters
    
    * refactor: lint next.config.js file
    
    * refactor: simplify not-found
    
    * refactor: add description to metadata
    
    ---------
    
    Co-authored-by: Kevin Yven Riexinger <info@kevinriexinger.de>
    
    * chore: lint not-found
    
    * ci: run vercel prod deployment on main
    
    * Add schools and kita (#4)
    
    * changed vercel prod to run on main
    
    * out
    
    * added custom 404 page
    
    * refactor: tidy up codebase a bit
    
    * changed parameters
    
    * added og:xxx
    
    * outsouceing header
    
    * added school page with example school
    
    * Update page.js
    
    * Update page.js
    
    * refactor: lint next.config.js file
    
    * refactor: simplify not-found
    
    * refactor: add description to metadata
    
    * added school data
    
    * ext workflow
    
    * renamed
    
    * working fadein animation
    
    * fucked up nameing
    
    * fine tuneing
    
    * i think i fixxed it
    
    * now i think i fixxed it
    
    * cleaned up
    
    * fix nr3
    
    * refactor: prevent breaking in buttons
    
    * refactor: rename Header prop
    
    * refactor: clarify variable names
    
    * removed old code
    
    * fix: set proper breakpoints
    
    * renoved internal
    
    * refactor: clean up code
    
    * removed loading animation
    
    * updated package-lock.json
    
    ---------
    
    Co-authored-by: Kevin Yven Riexinger <info@kevinriexinger.de>
    Co-authored-by: Kevin Riexinger <55785977+kevinriex@users.noreply.github.com>
    
    * refactor: lint
    
    * refactor: add prettier
    
    * ci: add dependabot
    
    * refactor: reneamed files and restructured code
    
    ---------
    
    Co-authored-by: Kevin Yven Riexinger <info@kevinriexinger.de>
    Co-authored-by: Kevin Riexinger <55785977+kevinriex@users.noreply.github.com>
    Co-authored-by: Kevin Yven Riexinger <github@me.kevinriex.de>

--- src/app/(components)/ServiceLink.jsx
+++ src/app/(components)/ServiceLink.jsx
@@ -1,4 +1,6 @@
-import OwnLink from "./OwnLink";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function ServiceLink({ name, href, desc, icon }) {
     return (
         <div className="col d-flex align-items-start">
@@ -8,7 +10,13 @@ export default function ServiceLink({ name, href, desc, icon }) {
             <div>
                 <h3 className="fs-4 text-body-emphasis">{name}</h3>
                 <p>{desc}</p>
-                <OwnLink href={href} button={true} />
+                <a href={href} className="btn btn-primary text-nowrap">
+                    {new RegExp(
+                        /(?:https:\/\/)(?<fqdn>(.*)(?:\.(de|com)))/,
+                        "gm"
+                    ).exec(href)?.groups?.fqdn ?? "Hier geht's lang.."}{" "}
+                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                </a>
             </div>
         </div>
     );
