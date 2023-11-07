7075780e627a2d9d31874f24950ccb2b4e5e9028
Kevin Yven Riexinger <github@me.kevinriex.de>
2023-11-07 15:40:55 +0100

    Datenschutz und Impressum

--- src/app/datenschutz/page.js
+++ src/app/datenschutz/page.js
@@ -2,175 +2,166 @@
 import StyledLink from "@/app/components/StyledLink";
 export default function DatenschutzPage() {
     return (
-        <div className="px-4 py-5 my-5 container">
+        <div className="px-4 py-5 my-5 container lead">
             <h1 className="py-5">Datenschutzerklärung</h1>
-            <h2>Allgemeiner Hinweis und Pflichtinformationen</h2>
-            <h3>Benennung der verantwortlichen Stelle</h3>
             <p>
-                Die verantwortliche Stelle für die Datenverarbeitung auf dieser
-                Website ist:
+                Wir, die Stadtverwaltung Ratingen, nehmen den Schutz Ihrer
+                persönlichen Daten sehr ernst und halten uns streng an alle
+                geltenden Gesetze und Vorschriften zum Datenschutz, insbesondere
+                an die Datenschutzgrundverordnung (DSGVO), das
+                Bundesdatenschutzgesetz (BDSG) und das Telemediengesetz (TMG).
+                Die folgenden Erläuterungen geben Ihnen einen Überblick darüber,
+                wie wir diesen Schutz sicherstellen und welche Daten wir zu
+                welchem Zweck verarbeiten.
             </p>
-            <p>
-                <span id="s3-t-firma">Stadt Ratingen</span>
-                <br />
-                <span id="s3-t-ansprechpartner">
-                    Der Datenschutzbeauftragte
-                </span>
-                <br />
-                <span id="s3-t-strasse">Minoritenstraße 2-6</span>
-                <br />
-                <span id="s3-t-plz">40878</span>{" "}
-                <span id="s3-t-ort">Ratingen</span>
-            </p>
-            <p>
-                Die verantwortliche Stelle entscheidet allein oder gemeinsam mit
-                anderen über die Zwecke und Mittel der Verarbeitung von
-                personenbezogenen Daten (z.B. Namen, Kontaktdaten o. Ä.).
-            </p>
-            <h3>Widerruf Ihrer Einwilligung zur Datenverarbeitung</h3>
-            <p>
-                Nur mit Ihrer ausdrücklichen Einwilligung sind einige Vorgänge
-                der Datenverarbeitung möglich. Ein Widerruf Ihrer bereits
-                erteilten Einwilligung ist jederzeit möglich. Für den Widerruf
-                genügt eine formlose Mitteilung per E-Mail. Die Rechtmäßigkeit
-                der bis zum Widerruf erfolgten Datenverarbeitung bleibt vom
-                Widerruf unberührt.
-            </p>
-            <h3>Recht auf Beschwerde bei der zuständigen Aufsichtsbehörde</h3>
-            <p>
-                Als Betroffener steht Ihnen im Falle eines
-                datenschutzrechtlichen Verstoßes ein Beschwerderecht bei der
-                zuständigen Aufsichtsbehörde zu. Zuständige Aufsichtsbehörde
-                bezüglich datenschutzrechtlicher Fragen ist der
-                Landesdatenschutzbeauftragte des Bundeslandes, in dem sich der
-                Sitz unseres Unternehmens befindet. Der folgende Link stellt
-                eine Liste der Datenschutzbeauftragten sowie deren Kontaktdaten
-                bereit:{" "}
-                <StyledLink href="https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html" arrow={true}/>
-                .
-            </p>
-            <h3>Recht auf Datenübertragbarkeit</h3>
-            <p>
-                Ihnen steht das Recht zu, Daten, die wir auf Grundlage Ihrer
-                Einwilligung oder in Erfüllung eines Vertrags automatisiert
-                verarbeiten, an sich oder an Dritte aushändigen zu lassen. Die
-                Bereitstellung erfolgt in einem maschinenlesbaren Format. Sofern
-                Sie die direkte Übertragung der Daten an einen anderen
-                Verantwortlichen verlangen, erfolgt dies nur, soweit es
-                technisch machbar ist.
-            </p>
-            <h3>Recht auf Auskunft, Berichtigung, Sperrung, Löschung</h3>
-            <p>
-                Sie haben jederzeit im Rahmen der geltenden gesetzlichen
-                Bestimmungen das Recht auf unentgeltliche Auskunft über Ihre
-                gespeicherten personenbezogenen Daten, Herkunft der Daten, deren
-                Empfänger und den Zweck der Datenverarbeitung und ggf. ein Recht
-                auf Berichtigung, Sperrung oder Löschung dieser Daten.
-                Diesbezüglich und auch zu weiteren Fragen zum Thema
-                personenbezogene Daten können Sie sich jederzeit über die im
-                Impressum aufgeführten Kontaktmöglichkeiten an uns wenden.
-            </p>
-            <h3>SSL- bzw. TLS-Verschlüsselung</h3>
-            <p>
-                Aus Sicherheitsgründen und zum Schutz der Übertragung
-                vertraulicher Inhalte, die Sie an uns als Seitenbetreiber
-                senden, nutzt unsere Website eine SSL-bzw. TLS-Verschlüsselung.
-                Damit sind Daten, die Sie über diese Website übermitteln, für
-                Dritte nicht mitlesbar. Sie erkennen eine verschlüsselte
-                Verbindung an der „https://“ Adresszeile Ihres Browsers und am
-                Schloss-Symbol in der Browserzeile.
-            </p>
-
-            <h2>Datenschutzbeauftragter</h2>
-            <p>Wir haben einen Datenschutzbeauftragten bestellt.</p>
-            <p>
-                Der Bürgermeister der Stadt Ratingen
-                <br />
-                Minoritenstraße 2-6
-                <br />
-                40878 Ratingen
-            </p>
-            <p>
-                Telefon: 02102 550 1000
-                <br />
-                E-Mail: datenschutz@ratingen.de
-            </p>
-
-            <h2>Server-Log-Dateien</h2>
-            <p>
-                In Server-Log-Dateien erhebt und speichert der Provider der
-                Website automatisch Informationen, die Ihr Browser automatisch
-                an uns übermittelt. Dies sind:
-            </p>
-            <ul>
-                <li>
-                    <p>Besuchte Seite auf unserer Domain</p>
-                </li>
-                <li>
-                    <p>Datum und Uhrzeit der Serveranfrage</p>
-                </li>
-                <li>
-                    <p>Browsertyp und Browserversion</p>
-                </li>
-                <li>
-                    <p>Verwendetes Betriebssystem</p>
-                </li>
-                <li>
-                    <p>Referrer URL</p>
-                </li>
-                <li>
-                    <p>Hostname des zugreifenden Rechners</p>
-                </li>
-                <li>
-                    <p>IP-Adresse</p>
-                </li>
-            </ul>
-            <p>
-                Es findet keine Zusammenführung dieser Daten mit anderen
-                Datenquellen statt. Grundlage der Datenverarbeitung bildet Art.
-                6 Abs. 1 lit. b DSGVO, der die Verarbeitung von Daten zur
-                Erfüllung eines Vertrags oder vorvertraglicher Maßnahmen
-                gestattet.
-            </p>
-
-            <h2>Font Awesome</h2>
-            <h3>Art und Umfang der Verarbeitung</h3>
-            <p>
-                Wir verwenden zur ordnungsgemäßen Bereitstellung der Inhalte
-                unserer Website Font Awesome des Anbieters Fonticons, Inc..
-            </p>
-            <h3>Zweck und Rechtsgrundlage</h3>
-            <p>
-                Der Einsatz von Font Awesome erfolgt auf Grundlage Ihrer
-                Einwilligung gemäß Art. 6 Abs. 1 lit. a. DSGVO und § 25 Abs. 1
-                TTDSG.
-            </p>
-            <p>
-                Wir beabsichtigen personenbezogenen Daten an Drittländer
-                außerhalb des Europäischen Wirtschaftsraums, insbesondere die
-                USA, zu übermitteln. In Fällen, in denen kein
-                Angemessenheitsbeschluss der Europäischen Kommission existiert
-                (z.B. in den USA) haben wir mit den Empfängern der Daten
-                anderweitige geeignete Garantien im Sinne der Art. 44 ff. DSGVO
-                vereinbart. Dies sind – sofern nicht anders angegeben –
-                Standardvertragsklauseln der EU-Kommission gemäß
-                Durchführungsbeschluss (EU) 2021/914 vom 4. Juni 2021. Eine
-                Kopie dieser Standardvertragsklauseln können Sie unter{" "}
-                <StyledLink href="https://eur-lex.europa.eu/legal-content/DE/TXT/HTML/?uri=CELEX:32021D0914&from=DE/" arrow={true}/>{" "}
-                einsehen.
-            </p>
-            <p>
-                Zudem holen wir vor einem solchen Drittlandtransfer Ihre
-                Einwilligung nach Art. 49 Abs. 1 Satz 1 lit. a. DSGVO ein, die
-                Sie über die Einwilligung im Consent Manager (oder sonstigen
-                Formularen, Registrierungen etc.) erteilen. Wir weisen Sie
-                darauf hin, dass bei Drittlandübermittlungen im Detail
-                unbekannte Risiken (z.B. die Datenverarbeitung durch
-                Sicherheitsbehörden des Drittlandes, deren genauer Umfang und
-                deren Folgen für Sie wir nicht kennen, auf die wir keinen
-                Einfluss haben und von denen Sie unter Umständen keine Kenntnis
-                erlangen) bestehen können.
-            </p>
+            <div>
+                <h2 className="pt-5">
+                    1. Verarbeitung personenbezogener Daten
+                </h2>
+                <p>
+                    Personenbezogene Daten werden von uns nur dann verarbeitet,
+                    wenn uns dies gesetzlich gestattet ist oder Sie uns Ihre
+                    Einwilligung erteilt haben. Rechtsgrundlage für diese
+                    Speicherung sind Art. 6 Abs. 1 lit. a EU-DSGVO, Art. 6 Abs.
+                    1 lit. b EU-DSGVO sowie Art. 6 Abs. 1 lit. e EU-DSGVO.
+                    Insbesondere werden die personenbezogenen Daten, die Sie uns
+                    bei der Nutzung des Bürgerportals zur Verfügung stellen nur
+                    für die Zwecke verwendet zu denen Sie Ihre Zustimmung
+                    erteilt haben. Nur mit einer erneuten Zustimmung ändert sich
+                    der Nutzungsbereich der Daten.
+                </p>
+            </div>
+            <div>
+                <h2 className="pt-5">
+                    2. Name und Kontaktdaten des Verantwortlichen
+                </h2>
+                <p>
+                    <strong>
+                        Bürgermeister der Stadt Ratingen, Klaus Pesch
+                    </strong>
+                    <br />
+                    Minoritenstraße 2-6
+                    <br />
+                    40878 Ratingen
+                    <br />
+                    Telefon: <StyledLink href="tel:02102 550-1000" />
+                    <br />
+                    E-Mail:{" "}
+                    <StyledLink href="mailto:buergermeister@ratingen.de" />
+                </p>
+            </div>
+            <div>
+                <h2 className="pt-5">
+                    3. Empfänger und Kategorien von Empfängern
+                </h2>
+                Ihre Daten werden an Dritte nicht weitergegeben, es sei denn
+                eine Weitergabe ist im Rahmen der Zuständigkeiten gesetzlich
+                erforderlich.
+            </div>
+            <div>
+                <h2 className="pt-5">4. Weiterleitung an Drittländer</h2>
+                Es besteht keine Absicht, personenbezogene Daten an einen
+                Empfänger in einem Drittland (Staaten außerhalb der Europäischen
+                Union und dem Europäischen Wirtschaftsraum) oder eine
+                internationale Organisation zu übermitteln.
+            </div>
+            <div>
+                <h2 className="pt-5">5. Sicherheit</h2>
+                Für die Sicherheit Ihrer Daten verwenden wir modernste
+                Internettechnologien wie z.B. eine SSL-Verschlüsselung. Für eine
+                sichere Speicherung Ihrer Daten werden unsere Systeme durch
+                Firewalls geschützt, die unberechtigte Zugriffe von außen
+                verhindern.
+            </div>
+            <div>
+                <h2 className="pt-5">6. Dauer der Speicherung</h2>
+                Logfiles speichern unter anderem die IP-Adresse, den verwendeten
+                Browser, Uhrzeit und Datum und das genutzte System eines
+                Seitenbesuchers. Bei uns werden nur anonymisierte IP-Adressen
+                von Besuchern der Website gespeichert. Im Übrigen speichern wir
+                Ihre Daten nur so lange, wie es für eine abschließende
+                Bearbeitung Ihres Anliegens notwendig ist. Ausgenommen hiervon
+                sind Daten, für die gesetzliche oder anderweitig vorgeschriebene
+                Aufbewahrungspflichten bestehen; diese werden für die Dauer der
+                jeweiligen Aufbewahrungsfrist gespeichert und im Anschluss
+                gelöscht.
+            </div>
+            <div>
+                <h2 className="pt-5">7. Betroffenenrechte</h2>
+                <h3 className="pt-4">Auskunftsrecht</h3>
+                Sie haben das Recht, von uns eine Bestätigung darüber zu
+                verlangen, ob Sie betreffende personenbezogene Daten verarbeitet
+                werden.
+                <h3 className="pt-4">
+                    Berichtigung / Löschung / Einschränkung der Verarbeitung
+                </h3>
+                Des Weiteren haben Sie das Recht, von uns zu verlangen, dass Sie
+                betreffende unrichtige personenbezogene Daten unverzüglich
+                berichtigt werden (Recht auf Berichtigung). Sie betreffende
+                personenbezogene Daten unverzüglich gelöscht werden (Recht auf
+                Löschung) und die Verarbeitung eingeschränkt wird (Recht auf
+                Einschränken der Verarbeitung).
+                <h3 className="pt-4">Recht auf Datenübertragbarkeit</h3>
+                Sie haben das Recht, Sie betreffende personenbezogene Daten, die
+                Sie uns bereitgestellt haben, in einem strukturierten, gängigen
+                und maschinenlesbaren Format zu erhalten und diese Daten einem
+                anderen Verantwortlichen zu übermitteln.
+                <h3 className="pt-4">Widerrufsrecht</h3>
+                Sie haben das Recht, Ihre Einwilligung jederzeit zu widerrufen.
+                Durch den Widerruf der Einwilligung wird die Rechtmäßigkeit der
+                aufgrund der Einwilligung bis zum Widerruf erfolgten
+                Verarbeitung nicht berührt.
+                <h3 className="pt-4">Widerspruchsrecht</h3>
+                Ist die Verarbeitung Sie betreffender personenbezogener Daten
+                für die Wahrnehmung einer Aufgabe, die im öffentlichen Interesse
+                liegt (Art. 6 Abs. 1 Buchst. e) DSGVO) oder zur Wahrung unserer
+                berechtigten Interessen (Art. 6 Abs. 1 Buchst. f) DSGVO)
+                erforderlich, steht Ihnen ein Widerspruchsrecht zu.
+                <h3 className="pt-4">Beschwerderecht</h3>
+                <p>
+                    Sind Sie der Ansicht, dass die Verarbeitung der Sie
+                    betreffenden personenbezogenen Daten gegen die DSGVO
+                    verstößt, haben Sie unbeschadet anderweitiger Rechtsbehelfe
+                    das Recht auf Beschwerde bei der zuständigen
+                    Aufsichtsbehörde:
+                </p>
+                <p>
+                    <strong>
+                        Landesbeauftragte/Landesbeauftragter für Datenschutz und
+                        Informationsfreiheit Nordrhein-Westfalen
+                    </strong>
+                    <br />
+                    Kavalleriestraße 2 – 4<br />
+                    40213 Düsseldorf
+                    <br />
+                    <strong>Telefon:</strong>{" "}
+                    <StyledLink href="tel:0211 38424-0" />
+                    <br />
+                    <strong>Telefax:</strong>{" "}
+                    <StyledLink href="tel:0211 38424-10" />
+                    <br />
+                    <strong>E-Mail:</strong>{" "}
+                    <StyledLink href="mailto:poststelle@ldi.nrw.de" />
+                    <br />
+                    <strong>Internet:</strong>{" "}
+                    <StyledLink href="https://www.ldi.nrw.de" />
+                    <br />
+                </p>
+            </div>
+            <div>
+                <h2 className="pt-5">
+                    8. Unser städtischer Datenschutzbeauftragter
+                </h2>
+                <p>
+                    Unseren städtischen Datenschutzbeauftragten erreichen Sie
+                    unter
+                    <StyledLink
+                        href="mailto:datenschutz@ratingen.de"
+                        arrow={true}
+                    />
+                    .
+                </p>
+            </div>
         </div>
     );
 }
