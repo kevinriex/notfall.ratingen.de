d39b3c4694ae55e2664a334bd1ddb432cd1ea999
Kevin Yven Riexinger <github@me.kevinriex.de>
2023-12-20 22:05:38 +0100

    added Eislaufzeiten & Kultureinrichtungen

--- src/app/kontakte/page.js
+++ src/app/kontakte/page.js
@@ -3,6 +3,7 @@ import { Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Contact from "../components/Contact";
 import HeroContact from "../components/HeroContact";
+import StyledLink from "../components/StyledLink";
 
 const Contacts = [
     { name: "Büro Bürgermeister", tel: "02102 550 1080" },
@@ -69,12 +70,95 @@ export default function ContactPage() {
     return (
         <>
             <Container className="px-4 py-5">
-                <HeroContact title="Kontaktadressen" />
+                <HeroContact title="Kontakt und Öffnungszeiten" />
                 {Contacts.map((contact, idx) => (
                     <Row className="g-4 py-5" key={idx}>
                         <Contact name={contact.name} tel={contact.tel} />
                     </Row>
                 ))}
+                <div className="col-lg-8 mx-auto">
+                    <h2>
+                        Öffnungszeiten der städtischen Kultureinrichtungen
+                        während der Feiertage
+                    </h2>
+                    <p className="lead mb-4 mt-4">
+                        Die Stadtbibliothek und die Stadtteilbibliotheken sind
+                        vom 24. bis 26. Dezember sowie an Silvester und Neujahr
+                        geschlossen. Die Zweigstelle Hösel bleibt vom 24.
+                        Dezember bis zum 10. Januar geschlossen. Die
+                        Touristinformation sowie der Ticketverkauf des
+                        Kulturamts sind zwischen Weihnachten und Neujahr vom 27.
+                        bis zum 29. Dezember geöffnet. Das Stadtarchiv ist zu
+                        den regulären Öffnungszeiten geöffnet, also am Mittwoch,
+                        27. Dezember, und am Donnerstag, 28. Dezember.
+                        <br />
+                        <br />
+                        Das Museum Ratingen ist am 24. und 25. Dezember sowie an
+                        Silvester und Neujahr geschlossen. Vom 26. bis 30.
+                        Dezember können die aktuellen Ausstellungen des Museums
+                        jeweils von 11 bis 17 Uhr besucht werden. Das
+                        Spielzeugmuseum mit der aktuellen Ausstellung „Busy Girl
+                        - Barbie macht Karriere“ ist für die Besucher dieses
+                        Jahr noch am 23., 26. und 30. Dezember geöffnet.
+                    </p>
+                    <h2>Eislaufzeiten in den Weihnachtsferien</h2>
+                    <p className="lead mb-4 mt-4">
+                        Die Ratinger Eissporthalle bietet auch über Weihnachten
+                        und natürlich generell in den Ferien ein tolles
+                        winterliches Programm für alle Eislauffans. Hier ein
+                        Überblick über Öffnungszeiten und Aktionen:
+                        <br />
+                        <br />
+                        <ul>
+                            <li>
+                                21. Dezember von 10 bis 14 Uhr und von 20 bis 22
+                                Uhr
+                            </li>
+                            <li>22. Dezember von 10 bis 17 Uhr</li>
+                            <li>
+                                23. Dezember von 11 bis 13 Uhr und von 14 bis 17
+                                Uhr (Familien-Disco)
+                            </li>
+                            <li>
+                                Heiligabend und am ersten Weihnachtstag
+                                geschlossen
+                            </li>
+                            <li>Zweiter Weihnachtstag von 10 bis 17 Uhr</li>
+                            <li>
+                                27. Dezember von 10 bis 17 Uhr und von 19 bis 21
+                                Uhr (Disco)
+                            </li>
+                            <li>
+                                28. Dezember von 10 bis 17 Uhr und von 20 bis 22
+                                Uhr (Oldie-Disco mit DJ Rainer)
+                            </li>
+                            <li>29. Dezember von 10 bis 17 Uhr</li>
+                            <li>
+                                30. Dezember von 11 bis 13 Uhr und von14 bis 17
+                                Uhr (Familien-Disco)
+                            </li>
+                            <li>
+                                Silvester von 12 bis 16 Uhr (Silvester-Laufzeit
+                                mit DJ Rainer)
+                            </li>
+                            <li>Neujahr geschlossen</li>
+                            <li>2. Januar von 10 bis 14 Uhr</li>
+                            <li>
+                                3. Januar von 10 bis 17 Uhr und von 19 bis 21
+                                Uhr (Disco)
+                            </li>
+                            <li>
+                                4. Januar von 10 bis 14 Uhr und von 20 bis 22
+                                Uhr (Oldie-Disco mit DJ Rainer)
+                            </li>
+                            <li>5. Januar von 10 bis 17 Uhr</li>
+                        </ul>
+                        <br />
+                        <br />
+                        Alle Informationen zu den Öffnungszeiten sind unter
+                        Telefon <StyledLink href="tel:02102 550 5250" /> oder <StyledLink href="/" text="ratingen.de" /> zu erfahren.
+                    </p>
+                </div>
             </Container>
         </>
     );
